60382f1b0e1dc07d74e11a1a82810cfda52ad8a6
Ajit D'Monte <ajitdmonte@gmail.com>
2020-06-21 23:44:09 +0530

    .

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def hello():
 
 def worker2():
     print "Start Flask"
-    application.run(host='0.0.0.0', port=8080)  
+    application.run()  
     
 if __name__ == "__main__":
     t = threading.Thread(target=worker2)
